3553916a827f509f67005efacc0cf3b4f9f176fc
DebajyotiBhatta <bdebajyoti22@gmail.com>
2025-02-01 17:11:55 +0000

    json

---
+++ .devcontainer/destructuring/d1_obj_to_json.js
@@ -0,0 +1,21 @@
+const course = {
+    coursename: "30_days_js",
+    price : 434,
+    instructor:"Hitesh Choudhary"
+}
+// course.instructor
+//destructuring 
+const {instructor} = course // or {instructor : in} instructor as in
+console.log(instructor)
+
+//json
+// {
+//     name: "dev", "name"
+//     email: "devahjk@njan.com" "email"
+// }
+
+[
+    {},
+    {},
+    {}
+]
