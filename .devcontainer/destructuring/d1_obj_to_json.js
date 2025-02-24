32998fb13526c0255878904db3f797a767536909
DebajyotiBhatta <bdebajyoti22@gmail.com>
2025-02-24 17:06:16 +0530

    Update d1_obj_to_json.js

--- .devcontainer/destructuring/d1_obj_to_json.js
+++ .devcontainer/destructuring/d1_obj_to_json.js
@@ -1,19 +1,46 @@
-const course = {
+onst course = {
     coursename: "30_days_js",
-    price : 434,
-    instructor:"Hitesh Choudhary"
+    price: 434,
+    instructor: "Hitesh Choudhary"
 }
-// course.instructor
-//destructuring 
-const {instructor} = course // or {instructor : in} instructor as in
-console.log(instructor)
 
-//json
-// {
-//     name: "dev", "name"
-//     email: "devahjk@njan.com" "email"
-// }
+// Object destructuring is a convenient way to extract properties from objects. Instead of writing:
 
+// javascriptCopyconst instructor = course.instructor
+// You can use destructuring syntax:
+// javascriptCopyconst {instructor} = course
+
+// You can also rename variables while destructuring using this syntax:
+
+ {instructor: in} = course  // The property 'instructor' is assigned to variable 'in'
+
+// Regarding JSON (JavaScript Object Notation):
+
+
+// JSON requires property names to be in double quotes
+// Your commented example can be written properly as:
+
+{
+    "name": "dev",
+    "email": "devahjk@njan.com"
+}
+Here are some additional examples of object destructuring that might be helpful:
+// Multiple properties at once
+const {coursename, price} = course
+
+// Destructuring with default values
+const {duration = "3 months"} = course  // duration will be "3 months" if not found in course
+
+// Nested destructuring
+const nested = {
+    course: {
+        name: "JavaScript",
+        details: {
+            level: "Beginner"
+        }
+    }
+}
+const {course: {details: {level}}} = nested
 [
     {},
     {},
